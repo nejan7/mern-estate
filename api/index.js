0cf33a44ec1aee0039d5b9935f6adab4d71347c7
nejan7 <nejanf.girl@gmail.com>
2024-05-01 13:28:00 +0330

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,20 @@
-import  express from "express";
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config(); //پسورد رو بردم توی دات انو
+
+// اگه پایگاه داده وصل شد یپام بده اگه نه ارور بده
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected To MongoDB!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
 const app = express();
 
-app.listen(3000, ()=>{
-    console.log('server is running on port 3000!');
-})
+app.listen(3000, () => {
+  console.log("server is running on port 3000!");
+});
