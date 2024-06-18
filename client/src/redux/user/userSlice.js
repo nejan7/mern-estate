f98a6f56c97c1db1dbdde17d47add1040600b2b0
nejan7 <nejanf.girl@gmail.com>
2024-06-18 18:56:14 +0330

    add redux toolkit

---
+++ client/src/redux/user/userSlice.js
@@ -0,0 +1,30 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  currentUser: null,
+  error: null,
+  loading: false,
+};
+
+const userSlice = createSlice({
+    name: 'user',
+    initialState,
+    reducers: { // نحوه تغییر وضعیت را بر اساس اکشن‌ها
+        signInStart: (state) => {
+            state.loading = true;
+        },
+        signInSuccess: (state, action) => {
+            state.currentUser = action.payload;
+            state.loading = false;
+            state.error = null;
+        },
+        signInFailure: (state, action) => {
+            state.error = action.payload;
+            state.loading = false;
+        }
+    }
+});
+
+export const { signInStart, signInSuccess, signInFailure } = userSlice.actions;
+
+export default userSlice.reducer; //to able to use it in redux store
