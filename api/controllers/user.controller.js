b3c99d01672e27d75c6e12ddaef89f8b4fee3152
nejan7 <nejanf.girl@gmail.com>
2024-07-03 18:15:46 +0330

    add delete user functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -37,3 +37,15 @@ export const updateUser = async(req, res, next) => {
     next(error);
   }
 };
+//delete function that call in route
+export const deleteUser = async (req, res, next) => {
+  if (req.user.id !== req.params.id) //pramp is id: -- get it on verifyUser - req.user from jwt
+    return next(errorHandler(401, 'You can only delete your own account!'));
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.clearCookie('access_token');//delete cookie after delete acount
+    res.status(200).json('User has been deleted!');
+  } catch (error) {
+    next(error);
+  }
+};
