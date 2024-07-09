3cf4fd5b96b6cd2c2f2428d4a93cb2592db32906
nejan7 <nejanf.girl@gmail.com>
2024-07-10 00:47:30 +0330

    create get user listings api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,7 +1,7 @@
-import bcryptjs from 'bcryptjs';
-import User from '../models/user.model.js';
-import { errorHandler } from '../utils/error.js';
-
+import bcryptjs from "bcryptjs";
+import User from "../models/user.model.js";
+import { errorHandler } from "../utils/error.js";
+import Listing from "../models/listing.model.js";
 
 export const test = (req, res) => {
   res.json({
@@ -9,9 +9,9 @@ export const test = (req, res) => {
   });
 };
 
-export const updateUser = async(req, res, next) => {
+export const updateUser = async (req, res, next) => {
   if (req.user.id !== req.params.id)
-    return next(errorHandler(401, 'You can only update your own account!'));
+    return next(errorHandler(401, "You can only update your own account!"));
   try {
     if (req.body.password) {
       req.body.password = bcryptjs.hashSync(req.body.password, 10);
@@ -20,14 +20,15 @@ export const updateUser = async(req, res, next) => {
     const updatedUser = await User.findByIdAndUpdate(
       req.params.id,
       {
-        $set: { // they can update what?
+        $set: {
+          // they can update what?
           username: req.body.username,
           email: req.body.email,
           password: req.body.password,
           avatar: req.body.avatar,
         },
       },
-      { new: true }//update new data with prevous data
+      { new: true } //update new data with prevous data
     );
 
     const { password, ...rest } = updatedUser._doc;
@@ -39,13 +40,28 @@ export const updateUser = async(req, res, next) => {
 };
 //delete function that call in route
 export const deleteUser = async (req, res, next) => {
-  if (req.user.id !== req.params.id) //pramp is id: -- get it on verifyUser - req.user from jwt
-    return next(errorHandler(401, 'You can only delete your own account!'));
+  if (req.user.id !== req.params.id)
+    //pramp is id: -- get it on verifyUser - req.user from jwt
+    return next(errorHandler(401, "You can only delete your own account!"));
   try {
     await User.findByIdAndDelete(req.params.id);
-    res.clearCookie('access_token');//delete cookie after delete acount
-    res.status(200).json('User has been deleted!');
+    res.clearCookie("access_token"); //delete cookie after delete acount
+    res.status(200).json("User has been deleted!");
   } catch (error) {
     next(error);
+  }
+};
+
+export const getUserListings = async (req, res, next) => {
+  if (req.user.id === req.params.id) {
+    try{
+      const listings = await Listing.find({ userRef: req.params.id });
+      res.status(200).json(listings);
+    }
+    catch(error){
+      next(error)
+    }
+  } else {
+    return next(errorHandler(401, "You can only view your own listings!"));
   }
 };
