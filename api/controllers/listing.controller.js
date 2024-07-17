b7b5b800e939547b09d9b37d9804c95a0d304571
nejan7 <nejanf.girl@gmail.com>
2024-07-18 00:30:35 +0330

    create search api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -55,11 +55,61 @@ export const getListing = async (req, res, next) => {
   try {
     const listing = await Listing.findById(req.params.id);
     if (!listing) {
-      return next(errorHandler(404, 'Listing not found!'));
+      return next(errorHandler(404, "Listing not found!"));
     }
     res.status(200).json(listing);
   } catch (error) {
     next(error);
   }
-  
+};
+//getListings------------------------------------------------
+export const getListings = async (req, res, next) => {
+  try {
+    const limit = parseInt(req.query.limit) || 9;//if dint have querry return 9
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    let offer = req.query.offer;
+
+    if (offer === undefined || offer === 'false') {
+      offer = { $in: [false, true] };//اگه افر رو انتخاب نکرده بود و سرچ کرد همه ی اف خورده یا نخورده ها نمایش داده بشن
+    }
+
+    let furnished = req.query.furnished;
+
+    if (furnished === undefined || furnished === 'false') {
+      furnished = { $in: [false, true] };
+    }
+    let parking = req.query.parking;
+
+    if (parking === undefined || parking === 'false') {
+      parking = { $in: [false, true] };
+    }
+
+    let type = req.query.type;
+
+    if (type === undefined || type === 'all') {
+      type = { $in: ['sale', 'rent'] }; //if rent or sale not selected
+    }
+
+    const searchTerm = req.query.searchTerm || '';
+
+    const sort = req.query.sort || 'createdAt';
+
+    const order = req.query.order || 'desc';
+
+    const listings = await Listing.find({
+      name: { $regex: searchTerm, $options: 'i' }, //regex ise a builting functionality for mongodb. search the rest of sentesnce
+      //$options: 'i' means dont care about uppercase and lowercase
+      offer,
+      furnished,
+      parking,
+      type,
+    })
+      .sort({ [sort]: order })
+      .limit(limit)
+      .skip(startIndex);
+
+    return res.status(200).json(listings);
+  } catch (error) {
+    next(error);
   }
+};
